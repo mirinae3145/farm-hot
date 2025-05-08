55fa7b5069c724243a298cf7a48e023e55d5ba6d
Shin Taeha <mirinae3145@g.skku.edu>
2025-05-08 17:18:12 +0900

    skip calib

--- arduino/IMU/MPU6050_fusion/MPU6050_fusion.ino
+++ arduino/IMU/MPU6050_fusion/MPU6050_fusion.ino
@@ -1,66 +1,25 @@
-/*
-  MPU6050 DMP6
-
-  Digital Motion Processor or DMP performs complex motion processing tasks.
-  - Fuses the data from the accel, gyro, and external magnetometer if applied, 
-  compensating individual sensor noise and errors.
-  - Detect specific types of motion without the need to continuously monitor 
-  raw sensor data with a microcontroller.
-  - Reduce workload on the microprocessor.
-  - Output processed data such as quaternions, Euler angles, and gravity vectors.
-
-  The code includes an auto-calibration and offsets generator tasks. Different 
-  output formats available.
-
-  This code is compatible with the teapot project by using the teapot output format.
-
-  Circuit: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
-  depends on the MPU6050's INT pin being connected to the Arduino's
-  external interrupt #0 pin.
-
-  The teapot processing example may be broken due FIFO structure change if using DMP
-  6.12 firmware version. 
-    
-  Find the full MPU6050 library documentation here:
-  https://github.com/ElectronicCats/mpu6050/wiki
-
-*/
-
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
-//#include "MPU6050_6Axis_MotionApps612.h" // Uncomment this library to work with DMP 6.12 and comment on the above library.
 
-/* MPU6050 default I2C address is 0x68*/
-MPU6050 mpu;
-//MPU6050 mpu(0x69); //Use for AD0 high
-//MPU6050 mpu(0x68, &Wire1); //Use for AD0 low, but 2nd Wire (TWI/I2C) object.
+MPU6050 mpu(0x68);
 
-/* OUTPUT FORMAT DEFINITION-------------------------------------------------------------------------------------------
-- Use "OUTPUT_READABLE_QUATERNION" for quaternion commponents in [w, x, y, z] format. Quaternion does not 
-suffer from gimbal lock problems but is harder to parse or process efficiently on a remote host or software 
-environment like Processing.
+#define USE_PRESET_OFFSETS
 
-- Use "OUTPUT_READABLE_EULER" for Euler angles (in degrees) output, calculated from the quaternions coming 
-from the FIFO. EULER ANGLES SUFFER FROM GIMBAL LOCK PROBLEM.
+#ifdef USE_PRESET_OFFSETS
+  #define XG_OFFSET 204
+  #define YG_OFFSET 1456
+  #define ZG_OFFSET 1270
+  #define XA_OFFSET 53
+  #define YA_OFFSET -31
+  #define ZA_OFFSET 50
+#endif
 
-- Use "OUTPUT_READABLE_YAWPITCHROLL" for yaw/pitch/roll angles (in degrees) calculated from the quaternions
-coming from the FIFO. THIS REQUIRES GRAVITY VECTOR CALCULATION.
-YAW/PITCH/ROLL ANGLES SUFFER FROM GIMBAL LOCK PROBLEM.
-
-- Use "OUTPUT_READABLE_REALACCEL" for acceleration components with gravity removed. The accel reference frame
-is not compensated for orientation. +X will always be +X according to the sensor.
-
-- Use "OUTPUT_READABLE_WORLDACCEL" for acceleration components with gravity removed and adjusted for the world
-reference frame. Yaw is relative if there is no magnetometer present.
-
--  Use "OUTPUT_TEAPOT" for output that matches the InvenSense teapot demo. 
--------------------------------------------------------------------------------------------------------------------------------*/ 
 // #define OUTPUT_READABLE_YAWPITCHROLL
-//#define OUTPUT_READABLE_QUATERNION
+#define OUTPUT_READABLE_QUATERNION
 //#define OUTPUT_READABLE_EULER
 //#define OUTPUT_READABLE_REALACCEL
 //#define OUTPUT_READABLE_WORLDACCEL
-#define OUTPUT_TEAPOT
+// #define OUTPUT_TEAPOT
 
 int const INTERRUPT_PIN = 2;  // Define the interruption #0 pin
 bool blinkState;
@@ -127,42 +86,42 @@ void setup() {
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
 
-  /* Supply your gyro offsets here, scaled for min sensitivity */
-  mpu.setXGyroOffset(0);
-  mpu.setYGyroOffset(0);
-  mpu.setZGyroOffset(0);
-  mpu.setXAccelOffset(0);
-  mpu.setYAccelOffset(0);
-  mpu.setZAccelOffset(0);
-
-  /* Making sure it worked (returns 0 if so) */ 
-  if (devStatus == 0) {
-    mpu.CalibrateAccel(6);  // Calibration Time: generate offsets and calibrate our MPU6050
+  #ifdef USE_PRESET_OFFSETS
+    mpu.setXGyroOffset(XG_OFFSET);
+    mpu.setYGyroOffset(YG_OFFSET);
+    mpu.setZGyroOffset(ZG_OFFSET);
+    mpu.setXAccelOffset(XA_OFFSET);
+    mpu.setYAccelOffset(YA_OFFSET);
+    mpu.setZAccelOffset(ZA_OFFSET);
+  #else
+    mpu.setXGyroOffset(0);
+    mpu.setYGyroOffset(0);
+    mpu.setZGyroOffset(0);
+    mpu.setXAccelOffset(0);
+    mpu.setYAccelOffset(0);
+    mpu.setZAccelOffset(0);
+    mpu.CalibrateAccel(6);
     mpu.CalibrateGyro(6);
-    Serial.println("These are the Active offsets: ");
-    mpu.PrintActiveOffsets();
-    Serial.println(F("Enabling DMP..."));   //Turning ON DMP
-    mpu.setDMPEnabled(true);
+  #endif
 
-    /*Enable Arduino interrupt detection*/
-    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-    Serial.println(F(")..."));
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), DMPDataReady, RISING);
-    MPUIntStatus = mpu.getIntStatus();
+  Serial.println("These are the Active offsets: ");
+  mpu.PrintActiveOffsets();
+  delay(1000);
 
-    /* Set the DMP Ready flag so the main loop() function knows it is okay to use it */
-    Serial.println(F("DMP ready! Waiting for first interrupt..."));
-    DMPReady = true;
-    packetSize = mpu.dmpGetFIFOPacketSize(); //Get expected DMP packet size for later comparison
-  } 
-  else {
-    Serial.print(F("DMP Initialization failed (code ")); //Print the error code
-    Serial.print(devStatus);
-    Serial.println(F(")"));
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-  }
+  Serial.println(F("Enabling DMP..."));   //Turning ON DMP
+  mpu.setDMPEnabled(true);
+
+  /*Enable Arduino interrupt detection*/
+  Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+  Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+  Serial.println(F(")..."));
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), DMPDataReady, RISING);
+  MPUIntStatus = mpu.getIntStatus();
+
+  /* Set the DMP Ready flag so the main loop() function knows it is okay to use it */
+  Serial.println(F("DMP ready! Waiting for first interrupt..."));
+  DMPReady = true;
+  packetSize = mpu.dmpGetFIFOPacketSize(); //Get expected DMP packet size for later comparison
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
